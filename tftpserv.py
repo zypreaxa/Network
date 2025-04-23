2b4472da6f2bf4c40cf6234ed1eac754c43de04d
Rimantas Liulys <rimantas.liulys@student.ucll.be>
2025-04-23 16:04:36 +0200

    Final working server code

--- tftpserv.py
+++ tftpserv.py
@@ -1,9 +1,6 @@
-import tftpy
+from tftpy import TftpServer
+import logging
 
-# Set the TFTP server directory to the one you just created
-tftp_folder = "C:\\TFTP-Server"  # Make sure to use double backslashes \\
-
-# Start the TFTP server
-server = tftpy.TftpServer(tftp_folder)
-print(f"Starting TFTP Server at 0.0.0.0:69 serving from {tftp_folder}")
-server.listen("0.0.0.0", 69)
+logging.basicConfig(level=logging.DEBUG)
+server = TftpServer('.')  # Change this!
+server.listen('0.0.0.0', 69)
