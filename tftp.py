a4d4f61bd9aab5dc3532f01b7120cecdd6efaf7a
Rimantas Liulys <rimantas.liulys@student.ucll.be>
2025-04-24 00:01:18 +0200

    minor adj

--- tftp.py
+++ tftp.py
@@ -2,5 +2,5 @@ from tftpy import TftpServer
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-server = TftpServer('.')  # Change this!
+server = TftpServer('.') 
 server.listen('0.0.0.0', 69)
